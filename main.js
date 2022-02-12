7fcc4e0b44c95b539bddc743306530a36f3ebc85
Michał Komar <michal.komar@miquido.com>
2022-02-12 21:28:17 +0100

    fix space texture

--- main.js
+++ main.js
@@ -9,15 +9,14 @@ import redPlanetUrl from './textures/red_planet.jpeg';
 import greenPlanetUrl from './textures/green_planet.png';
 import purplePlanetUrl from './textures/purple_planet.png';
 import sunUrl from './textures/sun.jpeg';
-import spaceUrl from './textures/space.jpg';
 
-const spaceImageUrl = spaceUrl.split('/')[2];
+const spaceImageUrl = 'https://i.imgur.com/gLGNnkp.jpeg';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const cubeTextureLoader = new THREE.CubeTextureLoader();
-scene.background = cubeTextureLoader.setPath('assets/').load([spaceImageUrl, spaceImageUrl, spaceImageUrl, spaceImageUrl, spaceImageUrl ,spaceImageUrl]);
+scene.background = cubeTextureLoader.load([spaceImageUrl, spaceImageUrl, spaceImageUrl, spaceImageUrl, spaceImageUrl ,spaceImageUrl]);
 const textureLoader = new THREE.TextureLoader();
 
 const ambientLight = new THREE.AmbientLight(0xadadad);
